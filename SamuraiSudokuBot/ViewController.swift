2afa4bd97d6215794ce1a96e0967b289de43f2f8
Isaac Benham <isaac.benham@gmail.com>
2016-04-21 13:50:37 -0400

    Initial Commit

---
+++ SamuraiSudokuBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamuraiSudokuBot
+//
+//  Created by Isaac Benham on 4/21/16.
+//  Copyright © 2016 Isaac Benham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
